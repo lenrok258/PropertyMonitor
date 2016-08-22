c95d8928938cb0dcb60720a15e8355607b8ec672
Kornel Zemla <kornelzemla@gmail.com>
2016-08-22 20:25:44 +0200

    Extracting email password to separate config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,19 @@
 var fs = require('fs');
+var _ = require('lodash')
 
-var config;
+var configs = {};
 
-function getSetting(name){
-	config = JSON.parse(fs.readFileSync(`${__dirname}/config.json`, 'utf8'));
-    return config[name];
+function getSetting(name) {
+    loadIfNeeded();
+    return configs[name];
+}
+
+function loadIfNeeded() {
+    if (_.isEmpty(configs)) {
+        var config = JSON.parse(fs.readFileSync(`${__dirname}/config.json`, 'utf8'));
+        var configSecret = JSON.parse(fs.readFileSync(`${__dirname}/config_secret.json`, 'utf8'));
+        configs = _.merge({}, config, configSecret)
+    }
 }
 
 exports.getSetting = getSetting;
